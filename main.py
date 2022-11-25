a45e9c6159a7566587021fa476cbbb87e15a775a
Guilherme Bernardo <guilhermebernardo@id.uff.br>
2022-11-25 19:45:50 -0300

    feat: add folders and menu functins

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
 if __name__ == "__main__":
     # Seus prints de teste aqui
+    print(get_product_by_id(28))
+    print(get_products_by_type('drink'))
+    print(menu_report())
     ...
--- manegement/product_hendler/menu.py
+++ manegement/product_hendler/menu.py
@@ -1,32 +1,39 @@
-import original_menu from 
+from .original_menu import original_products
 
-def get_product_by_id(_id: int) -> dict:
-    # Caso o produto referente ao id seja encontrado no menu, retorná-lo.
-    # Caso o produto referente ao id não seja encontrado no menu, retornar um dicionário vazio.
-    ...
+def get_product_by_id(id: int) -> dict:
 
-def get_products_by_type(type: str) -> list:
-    # Caso nenhum produto referente ao tipo seja encontrado, retornar uma lista vazia.
-    # Caso encontrados, deve ser retornada uma lista contendo todos os produtos do tipo especificado.
-    ...
+    for product in original_products:  
+        _id = product.values()
+        if _id == id:    
+            return product
+    return {}
+
+def get_products_by_type(search_type: str) -> list:
+
+    for product in original_products:
+        type = product.values()
+        if type == search_type:
+            return [product]
+    return []
 
 def menu_report() -> str:
     # Você deverá montar um report sobre algumas características do menu de produtos:
         # Product Count: Contagem do total de produtos do menu.
-        # Average Price: Média dos preços de todos os produtos do menu, arredondada para 2 casas decimais no máximo.
-        # Most Common Type: O tipo de produto mais comum, ou seja, o tipo que contém maior quantidade de produtos no menu.
     def get_product_count():
         ...
+        # Average Price: Média dos preços de todos os produtos do menu, arredondada para 2 casas decimais no máximo.
     def get_average_price():
         ...
+        # Most Common Type: O tipo de produto mais comum, ou seja, o tipo que contém maior quantidade de produtos no menu.
     def get_most_common_type():
         ...
 
-    
     report = {
-        "Product Count":"",
-        "Average Price":"",
-        "Most Common Type":""
+        "Product Count":get_product_count,
+        "Average Price":get_average_price,
+        "Most Common Type":get_most_common_type
     }
+    
+    return str(report)
     # O retorno deve ser uma string formatada exatamente como no exemplo abaixo, com o devido dinamismo de cada característica:
         # "Products Count: <contagem_de_produtos> - Average Price: $<preco_medio> - Most Common Type: <tipo_mais_comum>"
